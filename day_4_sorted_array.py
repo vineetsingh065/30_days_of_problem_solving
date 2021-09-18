2b5f88704b6300fdafe349a7f3ee99842466f206
Vineet Singh <vineetsingh065@gmail.com>
2021-09-18 14:25:44 +0530

    Add files via upload

---
+++ day_4_sorted_array.py
@@ -0,0 +1,40 @@
+def prob(arr, q) :
+    
+    start = 0
+    end = len(arr) - 1
+    while(start <= end) :
+
+        mid = start + (end - start) // 2
+        
+        if(arr[mid] == q) :
+            return mid
+        
+        elif(arr[mid] >= arr[start]) :
+
+            if(arr[start] <= q and q <= arr[mid]):
+                end = mid - 1
+            else :
+                start = mid + 1
+            
+        else :
+            if(arr[end] >= q and q >= arr[mid]) :
+                start = mid + 1
+            else :
+                end = mid - 1
+
+    return -1   
+
+
+if __name__ == "__main__":
+
+	n = int(input())
+	arr = []
+	for i in range(0, n):
+		t = int(input())
+		arr.append(t)
+
+	no_of_queries = int(input())
+
+	for i in range(0, no_of_queries):
+		q = int(input())
+		print(prob(arr, q))
